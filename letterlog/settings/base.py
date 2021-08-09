84ea6377797d5350d96fa5d4567cd1236915758f
Dongmyeong Lee <dom9316@gmail.com>
2021-08-10 01:56:05 +0900

    Add: Gmail SMTP server

--- letterlog/settings/base.py
+++ letterlog/settings/base.py
@@ -140,3 +140,10 @@ CORS_ALLOW_HEADERS = (
     'x-csrftoken',
     'x-requested-with',
 )
+
+EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_USE_TLS       = True
+EMAIL_PORT          = 587
+EMAIL_HOST_USER     = os.environ.get('DJANGO_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
